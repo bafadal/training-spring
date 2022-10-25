b0f4c8c63bdb87921442daac176c3f6baece5215
Ahmad <ahmadbafadal10@gmail.com>
2022-10-25 21:29:03 +0700

    perubahan kode bafadal

--- online/src/main/java/com/toko/online/respository/ProductRepository.java
+++ online/src/main/java/com/toko/online/respository/ProductRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
   Product getProductByNamaProduct(String namaProduct);
   Product getProductByJenisProduct(String jenisProduct);
+
+  Product getProductByKodeProduct(String kodeProduct);
 }
